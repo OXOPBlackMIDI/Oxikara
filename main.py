b6b78afba2f81102f3094308782047536355d382
OXOP <164915721+0XOPx@users.noreply.github.com>
2025-12-23 21:01:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from boask import route, use, run_server, html_templates
 @route("/")
 def index(handler):
     return html_templates.render(
-        "index.html",
-        title="Oxikara",
-        description="""Oxikara is a modern MIDI Player made using C++ and GLFW, GLAW, and Vulkan 1.1 (for Kepler and newer GPUs).""",
+        "index.html"
     )
 
 
